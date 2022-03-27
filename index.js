4eb16604b8b613db9fdaa277d027e1ef44b083f8
JohVitor <jvagardizan@gmail.com>
2022-03-27 19:19:40 -0300

    ajuste

--- index.js
+++ index.js
@@ -6,28 +6,30 @@
   const diretoriosImg = require("./diretoriosImg");
 
   let messages = "<h1>Olá João,</h1> <p>Segue o relatório com os valores</p>";
-  const vmzDisneyPag1 = await webScraping(
-    "https://www.vmzviagens.com.br/ingressos/orlando/walt-disney-orlando?pagina=1",
-    'pag-disney-1'
-  );
-  const vmzDisneyPag2 = await webScraping(
-    "https://www.vmzviagens.com.br/ingressos/orlando/walt-disney-orlando?pagina=2",
-    'pag-disney-2'
-  );
-  const vmzBG = await webScraping(
-    "https://www.vmzviagens.com.br/ingressos/orlando/busch-gardens-tampa",
-    "pag-bg"
-  );
-  const vmzUS = await webScraping(
-    "https://www.vmzviagens.com.br/ingressos/orlando/universal-studios",
-    "pag-us"
-  );
 
-  messages += mensage(vmzDisneyPag1, 'pag-disney-1');
-  messages += mensage(vmzDisneyPag2, 'pag-disney-2');
-  messages += mensage(vmzBG, "pag-bg-3");
-  messages += mensage(vmzUS, "pag-us-4");
+  const listUrl = [
+    {
+      url: "https://www.vmzviagens.com.br/ingressos/orlando/walt-disney-orlando?pagina=1",
+      infoPag: "pag-disney-1",
+    },
+    {
+      url: "https://www.vmzviagens.com.br/ingressos/orlando/walt-disney-orlando?pagina=2",
+      infoPag: "pag-disney-2",
+    },
+    {
+      url: "https://www.vmzviagens.com.br/ingressos/orlando/busch-gardens-tampa",
+      infoPag: "pag-bg",
+    },
+    {
+      url: "https://www.vmzviagens.com.br/ingressos/orlando/universal-studios",
+      infoPag: "pag-us",
+    },
+  ];
 
+  for (const itemList of listUrl) {
+    let itens = webScraping(itemList.url, itemList.infoPag);
+    messages += mensage(await itens, itemList.infoPag);
+  }
   email(messages, await diretoriosImg());
 
   return;
